a2d00779294ed35d17779454e52ad881a25982fd
Jungjun <dalbicih9801@gmail.com>
2025-05-01 04:38:13 +0900

    modify week3 code:1

--- week-3/code/custom_erc20.sol
+++ week-3/code/custom_erc20.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.19;
 
 //ERC20 토큰을 라이브러리 없이 직접 만드는 예시
+
 interface ERC20Interface {
     function totalSupply() external view returns(uint);
     function balanceOf(address account) external view returns(uint balance);
@@ -22,18 +23,26 @@ contract ComitToken is ERC20Interface {
 
     mapping(address => uint) balance;
     mapping(address => mapping(address=>uint)) _allowance;
+    address public deployer;
 
+    //생성자 정보를 바탕으로 scan사이트 및 metamask 등에서 토큰으로 인식
     constructor() {
-        //생성자 정보를 바탕으로 scan사이트 및 metamask에서 토큰으로 인식
         symbol = "CMT";
         name = "Comit Coin";
         decimals = 18;
         _totalSupply = 1_000_001 * (10 ** decimals);
-        balance[msg.sender] = _totalSupply ;
-        emit Transfer(address(0), msg.sender, _totalSupply);
+        deployer = msg.sender;
+        balance[deployer] = _totalSupply ;
+        emit Transfer(address(0), deployer, _totalSupply);
     }
 
-    //토큰의 총 발행량을 출력
+    //초기 배포자만 권한 부여하는 모디파이어
+    modifier onlyOwner() {
+        require(msg.sender == deployer, "Not contract owner");
+        _;
+    }
+
+    //토큰의 총 발행량을 조회
     function totalSupply() public view returns(uint) {
         return _totalSupply;
     }
@@ -50,6 +59,22 @@ contract ComitToken is ERC20Interface {
         balance[receiver] += amount;
         emit Transfer(msg.sender, receiver, amount);
         return true;
+    }
+
+    //초기 배포자에 한해서 원하는 양만큼 토큰을 발행
+    function mint(address to, uint amount) public onlyOwner {
+        require(to != address(0), "Cannot mint to zero address");
+        balance[to] += amount;
+        _totalSupply += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    //내가 가진 토큰 원하는 양만큼 소각
+    function burn(uint amount) public {
+        require(balance[msg.sender] >= amount, "Insufficient balance to burn");
+        balance[msg.sender] -= amount;
+        _totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
     }
 
     //함수를 호출하는 주소의 토큰을 spender(주소)가 특정 양만큼 사용하도록 허락
